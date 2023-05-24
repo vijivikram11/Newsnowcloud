92ab986ce2423f2c560e4aca4192fcbd48f5beab
viji.jagadeeswari@progleasing.com <viji.jagadeeswari@progleasing.com>
2023-05-24 12:02:59 +0000

    cc

--- cloud.sql
+++ cloud.sql
@@ -1,1 +1,15 @@
-select * from Dim_TravelType
+with TargetResults as (
+      passenger.ID,
+      passenger.GENDER,
+      custtype.CUSTOMERID as CUSTOMERTYPEID, 
+      traveltype.TYPEID as TYPEOFTRAVELID,
+      passenger.CLASS,
+      passenger.SATISFACTION,
+      from survey.public.survey_data passenger
+    left join survey.public.Dim_CustomerType custtype
+      on passenger.CUSTOMERTYPE=custtype.CUSTOMERTYPE
+    left join survey.public.Dim_TravelType traveltype
+      on passenger.TYPEOFTRAVEL=traveltype.TYPEID  
+ )
+
+ select * from TargetResults
