52e3344810b3ef6a44a326babf824b854ea1a815
viji.jagadeeswari@progleasing.com <viji.jagadeeswari@progleasing.com>
2023-05-24 12:07:20 +0000

    cw

--- cloud.sql
+++ cloud.sql
@@ -1,14 +1,31 @@
-with TargetResults as (
+with passenger as (
+
+  select * from 'SURVEY.PUBLIC.SURVEY_DATA'
+  
+), 
+
+custtype as (
+
+  select * from 'SURVEY.PUBLIC.DIM_CUSTOMERTYPE'
+  
+), 
+
+traveltype as (
+
+  select * from 'SURVEY.PUBLIC.DIM_TRAVELTYPE'
+  
+), 
+TargetResults as (
       passenger.ID,
       passenger.GENDER,
       custtype.CUSTOMERID as CUSTOMERTYPEID, 
       traveltype.TYPEID as TYPEOFTRAVELID,
       passenger.CLASS,
       passenger.SATISFACTION,
-      from survey.public.survey_data passenger
-    left join survey.public.Dim_CustomerType custtype
+      from passenger
+    left join custtype
       on passenger.CUSTOMERTYPE=custtype.CUSTOMERTYPE
-    left join survey.public.Dim_TravelType traveltype
+    left join traveltype
       on passenger.TYPEOFTRAVEL=traveltype.TYPEID  
  )
 
